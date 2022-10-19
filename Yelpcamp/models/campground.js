dccfa96ca4bd345041d5b7a2d28a28fd86e0b9a7
saurabhcoder2536 <102011318+saurabhcoder2536@users.noreply.github.com>
2022-10-19 19:32:38 +0530

    YelpCamp may be a website where users can create and review campgrounds. In order to review or create a campground, you want to have an account. This project was designed using Node.js, Express, MongoDB, and Bootstrap. Passport.js was used to handle authentication. The Login Feature gives the user right to login to the website after creation of the account successfully for the website. The login process is on high priority. During the login process the user needs to put the User id and Password in order to access the website contents.

---
+++ Yelpcamp/models/campground.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    author :{
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
---
+++ Yelpcamp/routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+const users = require('../controllers/users');
+
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
+
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+    router.get('/logout', function(req, res, next) {
+        req.logout(function(err) {
+          if (err) { return next(err); }
+          res.redirect('/campgrounds');
+        });
+      });
+
+module.exports = router;
